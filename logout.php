838fd1c5cbf397de9d8ec19e121f19e977f7f0e5
Kelvin Lee <kahokelvinlee@gmail.com>
2018-05-15 21:07:30 -0700

    Added logout/user info nav to index.html

---
+++ logout.php
@@ -0,0 +1,17 @@
+<?php
+    session_start();
+
+    $methodType = $_SERVER['REQUEST_METHOD'];
+    $data = array("msg" => "$methodType");
+
+    if ($methodType === 'GET') {
+        session_destroy();
+        $data = array("msg" => "Logged out");
+
+    } else {
+        $data = array("msg" => "Error: only GET allowed");
+    }
+
+    echo json_encode($data, JSON_FORCE_OBJECT);
+
+?>
