f6575a7da6e3165bd83231295d26e81d07218034
Sammy <bigsaem@gmail.com>
2018-05-15 23:29:05 -0700

    nav bar on all pages and desktop recipe page update

--- GetRecipeID.php
+++ GetRecipeID.php
@@ -11,7 +11,7 @@ if ($methodType === 'GET') {
         $ingredients = $_GET["ingredients"];
 
         $requestString = "https://spoonacular-recipe-food-nutrition-v1.p.mashape.com/recipes/findByIngredients?fillIngredients=false&ingredients="
-        .$ingredients[0]."%2C+".$ingredients[1]."%2C+".$ingredients[2]."%2C+".$ingredients[3]."%2C+".$ingredients[4]."&limitLicense=false&number=5&ranking=2";
+        .$ingredients[0]."%2C+".$ingredients[1]."%2C+".$ingredients[2]."%2C+".$ingredients[3]."%2C+".$ingredients[4]."&limitLicense=false&number=6&ranking=2";
 
         //this requires Unirest. Unirest is needed to use the API. 
         $response = Unirest\Request::get($requestString,
